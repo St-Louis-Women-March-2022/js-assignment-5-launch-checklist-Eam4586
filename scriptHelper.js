03d11c09121b2f55d70b7d8875abeb3e4deef737
Elizabeth McMahon <elizabethmcmahon@Elizabeths-MacBook-Pro.local>
2022-07-12 21:16:44 -0500

    Fixed lots..
    We will see if it works!

--- scriptHelper.js
+++ scriptHelper.js
@@ -12,49 +12,64 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 <li>Distance from Earth: ${distance}</li>
                 <li>Number of Moons: ${moons}</li>
             </ol>
-            <img src="${image}"/>`;
-};
+            <img src="${imageUrl}">`;
+}
 
 function validateInput(event) {
-    let pilotNameInput = document.querySelector("input[name=pilotName]");
-    let copilotNameInput = document.querySelector("input[name=copilotName]");
-    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-    let cargoMassInput = document.querySelector("input[name=cargoMass]");
+    let inputNumber = Number(event);
+    if (inputNumber === ""){
+        return "empty";
+    }
 
-    if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "" ) {
-        alert("All fields are required!")};
+    else if (isNAN(inputNumber)){
+        return "not number";
+    }
 
-    if (isNAN(cargoMassInput.value) || (fuelLevelInput.value)) {
-        alert ("Cargo Mass and Fuel Levels must be entered as a number.")}
-
-    if (isNAN(pilotNameInput.value) || (copilotNameInput.value) === false) {
-        alert ("Pilot Name and CoPilot Name must be entered as a word.") }
-        event.preventDefault();
-         };
+    else if (isNAN(inputNumber)===false){
+        return "is a number"
+    }
+};
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
     element.setAttribute("pilotStatus", "${pilotNameInput} Ready");
     element.setAttribute("copilotStatus", "${copilotNameInput} Ready");
+
+let pilotNameInput = document.querySelector("input[name=pilotName]");
+let copilotNameInput = document.querySelector("input[name=copilotName]");
+let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+    if (validateInput (pilotNameInput) === "empty" || validateInput (copilotNameInput) === "empty" || validateInput (fuelLevelInput) === "empty" || validateInput (cargoMassInput) === "empty" ) {
+        alert("All fields are required!");}
+
+    else if (validateInput (cargoMassInput) === "not number" || validateInput (fuelLevelInput) === "not number") {
+        alert ("Cargo Mass and Fuel Levels must be entered as a number.");}
+
+    else if (validateInput (pilotNameInput) === "is a number" || validateInput (copilotNameInput) === "is a number"); {
+        alert ("Pilot Name and CoPilot Name must be entered as a word.");}
+
+    // else {    
    
-    if (fuelLevelInput<10,000){
-        element.setAttribute("fuelStatus",`There is not enough fuel for the journey.`);
-        element.setAttribute("launchStatus", `Shuttle not ready for launch`);
-        launchStatus.style.color = "red";
-        faultyItems.style.visibility = "visible";
-    }
+    //     if (fuelLevelInput<10,000){
+    //         element.setAttribute("fuelStatus",`There is not enough fuel for the journey.`);
+    //         element.setAttribute("launchStatus", `Shuttle not ready for launch`);
+    //         launchStatus.style.color = "red";
+    //         faultyItems.style.visibility = "visible";
+    //     }
 
-    else if (cargoMassInput>10,000){
-        element.setAttribute("launchStatus", `Shuttle not ready for launch`);
-        launchStatus.style.color = "red";
-        faultyItems.style.visibility = "visible";
-    }
+    //     else if (cargoMassInput>10,000){
+    //         element.setAttribute("launchStatus", `Shuttle not ready for launch`);
+    //         launchStatus.style.color = "red";
+    //         faultyItems.style.visibility = "visible";
+    //     }
 
-    else {
-        element.setAttribute("launchStatus", `Shuttle is ready for launch`);
-        launchStatus.style.color = "green";
+    //     else {
+    //         element.setAttribute("launchStatus", `Shuttle is ready for launch`);
+    //         launchStatus.style.color = "green";
+    //     }
+    // }
     }
-};
 
 async function myFetch() {
     fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
